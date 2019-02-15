c37f4c8aeb1a35c8ad2aa21474ba646e13ee8452
Howard Fraser <howardjfraser@gmail.com>
2019-02-15 14:38:36 +1100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+var url = require('url');
+var request = require('request');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', (process.env.PORT || 9001));
+
+app.get('/', function(req, res){
+  res.send('It works!');
+});
+
+app.post('/post', function(req, res){
+  var parsed_url = url.format({
+    pathname: 'https://api.genius.com/search',
+    query: {
+      access_token: process.env.GENIUS_ACCESS,
+      q: req.body.text
+    }
+  });
+
+  request(parsed_url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      var first_url = data.response.hits[0].result.url;
+
+      var body = {
+        response_type: "in_channel",
+        text: first_url
+      };
+
+      res.send(body);
+    }
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
