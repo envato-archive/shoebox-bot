2cee835d074010cf2ac424b344455882701ea667
Howard Fraser <howardjfraser@gmail.com>
2019-02-15 15:25:10 +1100

    Handle verification request.

--- index.js
+++ index.js
@@ -9,17 +9,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', (process.env.PORT || 9001));
 
+var VERIFICATION_TOKEN = process.env.SLACK_VERIFICATION_TOKEN || 'faketoken';
+
 app.get('/', function(req, res){
   res.send('It works!');
 });
 
-app.post('/event', function(req, res){
-  var body = {
-    response_type: "in_channel",
-    text: "hello, slack..."
-  };
+app.post('/post', function(req, res){
+  switch(req.body.type) {
+    case "url_verification":
+      var token = req.body.token;
+      var challenge = req.body.challenge;
 
-  res.send(body);
+      if (token === VERIFICATION_TOKEN) {
+        res.send({
+          challenge: challenge
+        });
+      } else {
+        res.status(403);
+        res.send('');
+      }
+    break;
+    default:
+      res.status(400);
+      res.send('');
+    break;
+  }
 });
 
 app.listen(app.get('port'), function() {
